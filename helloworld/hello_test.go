4aa0af441ef2a24534e8eed8f8d8cdd68689666e
IchiThe2nd <david.swensson@yahoo.com>
2024-11-30 15:56:10 -0800

    Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch main
    
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    
    	new file:   Arrrays/array.go
    
    	new file:   Interation/Iteration.go
    
    	new file:   Interation/iteration_test.go
    
    	deleted:    hello.go
    
    	new file:   helloworld/hello.go
    
    	renamed:    hello_test.go -> helloworld/hello_test.go
    
    	new file:   integers/integers_test.go

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 
+	})
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Frenchy", "French")
+		want := "Bon Jour, Frenchy"
+		assertCorrectMessage(t, got, want)
 	})
 }
 
