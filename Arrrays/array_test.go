e2346a9da02cbbd93faf9dbab9c15b822549f1bd
IchiThe2nd <david.swensson@yahoo.com>
2024-11-30 16:10:16 -0800

    modified:   Arrrays/array.go 	modified:   Arrrays/array_test.go

--- Arrrays/array_test.go
+++ Arrrays/array_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -16,12 +17,12 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	// t.Run("collections of any size", func(t *testing.T) {
-	// 	numbers := []int{1, 2, 3}
-	// 	got := Sum(numbers)
-	// 	want := 6
-	// 	if got != want {
-	// 		t.Errorf("got %d wanted %d given %v ", got, want, numbers)
-	// 	}
-	// })
+}
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v wanted %v ", got, want)
+	}
+
 }
