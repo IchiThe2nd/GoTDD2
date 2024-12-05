60db884444aa64d9b7fd8c58ff87643a42b62c23
IchiThe2nd <david.swensson@yahoo.com>
2024-12-05 15:11:12 -0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   Maps/maps_test.go

--- Maps/maps_test.go
+++ Maps/maps_test.go
@@ -18,6 +18,69 @@ func TestSearch(t *testing.T) {
 	})
 
 }
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+
+		assertDefinition(t, dictionary, word, definition)
+
+	})
+
+	t.Run("ecisting word", func(t *testing.T) {
+
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "New Test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("New word", func(t *testing.T) {
+
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+		assertError(t, err, ErrWordDoesNotExist)
+
+	})
+}
+
+// Assertions
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
@@ -30,16 +93,4 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got error %q wanted %q", got, want)
 	}
-}
-
-func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is a test")
-	want := "this is a test"
-	got, err := dictionary.Search("test")
-	if err != nil {
-		t.Fatal("should find added word", err)
-	}
-	assertStrings(t, got, want)
-
 }
