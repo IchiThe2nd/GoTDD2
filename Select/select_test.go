06b6444d9aed810619695dde6a80f63532d5440d
Ichi <Ichi@Sisu.com>
2024-12-10 20:35:06 -0400

    Deleted Olds

--- Select/select_test.go
+++ Select/select_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestRacer(t *testing.T) {
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.dev"
-
+	//var xys := "wfw"
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 
