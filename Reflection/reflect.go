378d26fc32ad61acdfc76c6fa5097b83544b237d
Ichi <Ichi@Sisu.com>
2024-12-13 20:33:38 -0400

    modified:   Reflection/reflect.go 	modified:   Reflection/reflect_test.go

--- Reflection/reflect.go
+++ Reflection/reflect.go
@@ -6,27 +6,29 @@ import (
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value //get field is a function variable set to reflect.Value ??
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index // getField calls reflect.Value on Val.Index becoming and Index(Slice)
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
 
 	}
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
-	}
+
 }
 
 func getValue(x interface{}) reflect.Value {
