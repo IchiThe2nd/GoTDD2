c7472f0baf69b510cf0c2edd1b0c6009e03250b3
Ichi <Ichi@Sisu.com>
2024-12-17 20:36:16 -0400

    Fuck import paths

--- clockface/math_test.go
+++ clockface/math_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/IchiThe2nd/GoTDD2/Maths/clockface"
+	"github.com/IchiThe2nd/GoTDD2/clockface"
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{x: 150, Y: 150 - 90}
+	want := clockface.Point{X: 150, Y: 150 - 90}
 	got := clockface.SecondHand(tm)
 
 	if got != want {
