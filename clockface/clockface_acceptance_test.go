2830e6bbb42b04dafa2afee5ae7e0aed6a401885
Ichi <Ichi@Sisu.com>
2024-12-19 20:43:39 -0400

    svgwwrite secon

--- clockface/clockface_acceptance_test.go
+++ clockface/clockface_acceptance_test.go
@@ -9,22 +9,35 @@ import (
 	"github.com/IchiThe2nd/GoTDD2/clockface"
 )
 
-func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+// zek xml-go
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
 
-	b := bytes.Buffer{}
-	clockface.SVGWriter(&b, tm)
-
-	svg := Svg{}
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	x2 := "150"
-	y2 := "60"
-
-	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
-			return
-		}
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{///}
 	}
-	t.Errorf("expected to find the second hand  x2 of %+v and y2 of %+v ,in the vg file %v", x2, y2, b.String())
 }
