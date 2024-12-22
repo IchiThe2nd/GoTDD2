7a4279974cecf849a0bf6f2625671721c7f6b269
Ichi <Ichi@Sisu.com>
2024-12-21 20:36:40 -0400

    refractor newpost next

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -4,17 +4,27 @@ import (
 	"io/fs"
 )
 
-type Post struct {
-}
-
-func NewPostFromFS(filesystem fs.FS) ([]Post, error) {
-	dir, err := fs.ReadDir(filesystem, ".")
+func NewPostFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
 	if err != nil {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err //clarify failing if one file fails or not
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName) //opened a file
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile) //
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -1,6 +1,9 @@
 package blogposts_test
 
 import (
+	"errors"
+	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -8,18 +11,45 @@ import (
 	//blogposts "github.com/IchiThe2nd/GoTDD2/blogposts
 )
 
-func TestNewBlogPost(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello worl.md": {Data: []byte("hi")},
-		"hello-world2":  {Data: []byte("hola")},
-	}
-	posts, err := blogposts.NewPostFromFS(fs)
+type StubFailinngFS struct {
+}
 
+func (s StubFailinngFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh nos I always fail")
+}
+
+func TestNewBlogPost(t *testing.T) {
+	const ( // make some semi constants
+		firstBody = `Title: Post 1
+Description : Description 1`
+		secondBody = `Title: Post 2
+Description : Description 2`
+	)
+
+	fs := fstest.MapFS{ //make  a filesystem
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostFromFS(fs) //pull post from filesystm
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts,wanted %d posts", len(posts), len(fs))
+	}
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+	})
+}
+
+///assertiions
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v,wanted %+v", got, want)
+
 	}
 }
